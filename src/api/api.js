074d24cde5c90c77a58b17117847ff16a3eebe52
Aynura <aynura.asadova95@gmail.com>
2022-09-30 16:19:40 +0400

    updated

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-  baseURL:  `http://localhost:3002`,
+  baseURL:  `https://my-json-server.typicode.com/aynuraasadova/todo-list-redux`,
   headers: { "Content-Type": "application/json" },
 });
 
